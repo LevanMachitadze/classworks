cd8dbffd6948b635e9359cdda525bcd6213598ca
Levani <machitadzelevan@gmail.com>
2024-11-26 03:29:34 +0400

    mainPage

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,26 @@ export default function Home() {
     <main className='flex flex-col items-center justify-center h-screen p-24'>
       <div className='text-center'>
         <h1 className='uppercase text-white tracking-[1.5rem] font-medium py-8 text-2xl'>
-          Our cinematic
+          Welcome To Relax Room
         </h1>
         <div className='flex items-center justify-center space-x-4'>
           <button className='px-8 py-6 mx-4 bg-transparent border border-white text-xs text-white uppercase transition duration-20 hover:bg-white hover:text-black'>
-            our work
+            About Us
           </button>
           <button className='px-8 py-6 mx-4 bg-transparent border border-white text-xs text-white uppercase transition duration-20 hover:bg-white hover:text-black'>
-            our story
+            Services
+          </button>
+          <button className='px-8 py-6 mx-4 bg-transparent border border-white text-xs text-white uppercase transition duration-20 hover:bg-white hover:text-black'>
+            Contact
+          </button>
+          <button className='px-8 py-6 mx-4 bg-transparent border border-white text-xs text-white uppercase transition duration-20 hover:bg-white hover:text-black'>
+            Galerry
           </button>
         </div>
       </div>
       <video
         src='/video.mp4'
         autoPlay
-        muted
         loop
         className='absolute -z-10 h-full w-full object-cover inset-0'
       />
